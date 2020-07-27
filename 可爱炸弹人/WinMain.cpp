315fdd71c84c271809d8aa3c91d792b3d6ab7300
Timothy <liuxf19@163.com>
2020-07-27 14:51:40 +0800

    y改变了目录结构

--- 可爱炸弹人/WinMain.cpp
+++ 可爱炸弹人/WinMain.cpp
@@ -80,7 +80,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.hInstance = hInstance;
     wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
     wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
     wcex.lpszMenuName = NULL;
     wcex.lpszClassName = c_lpszWndClassName;
     wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);

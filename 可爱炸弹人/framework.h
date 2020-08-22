421e63d88916b873583f6c512ad808cd2bfa81b8
Timothy <liuxf19@163.com>
2020-08-22 14:53:02 +0800

    优化了界面

--- 可爱炸弹人/framework.h
+++ 可爱炸弹人/framework.h
@@ -28,6 +28,16 @@
 #include <fstream>
 #include <type_traits>
 
+#ifdef _M_IX86
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#elif _M_IA64
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='ia64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#elif _M_X64
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#else
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif
+
 #ifdef _UNICODE
 #define _tstring wstring
 #define _tistringstream wistringstream
